dc4b639e7323e976e005cfcf9a465f4880da2a9e
rafaelaandradec <rafaelaandrade.costa@gmail.com>
2023-06-01 00:01:45 -0300

    "Alteração no task form"

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -6,7 +6,7 @@ import { Task } from './task';
 })
 export class TaskService {
   tasks: Task[] = [
-    { id: 1, description: 'Tarefa 1', completed: false },
+    { id: 1, description: 'Tarefa 1', completed: true },
     { id: 2, description: 'Tarefa 2', completed: false },
     { id: 3, description: 'Tarefa 3', completed: true },
     { id: 4, description: 'Tarefa 4', completed: false },
@@ -25,20 +25,27 @@ export class TaskService {
   }
 
   getById(id: number){
-    const task = this.tasks.find((value) => value.id == id);
+    const task = this.tasks.find((value) => value.id == id); //método find vai devolver apenas uma tarefa
     return task;
   }
 
-  save(task: Task){
-    if (task.id){
+  save(task: Task) {
+    if (task.id) {
       const taskArr = this.getById(task.id);
+      if (taskArr) { // Verifica se taskArr é definido antes de acessar suas propriedades
         taskArr.description = task.description;
         taskArr.completed = task.completed;
-    } else {
-      const lastId = this.tasks[this.tasks.length-1].id;
-        task.id = lastId + 1;
-        task.completed = false;
-        this.tasks.push(task);
+      }
+    } else { //se não tem id vai gerar um novo aqui
+      const lastId = this.tasks[this.tasks.length - 1]?.id || 0; // Obtém o último ID da tarefa ou define como 0 se a lista estiver vazia
+      task.id = lastId + 1;
+      task.completed = false;
+      this.tasks.push(task);
     }
+  }
+
+  delete(id : number) {
+    const taskIndex = this.tasks.findIndex((value) => value.id == id);
+    this.tasks.splice(taskIndex, 1);
   }
 }
--- src/app/tasks/task-form/task-form.component.ts
+++ src/app/tasks/task-form/task-form.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { TaskService } from './../shared/task.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Task } from './../shared/task';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-task-form',
   templateUrl: './task-form.component.html',
   styleUrls: ['./task-form.component.css']
 })
-export class TaskFormComponent {
+export class TaskFormComponent implements OnInit {
+  task: Task = new Task();
+  title: string = 'Nova Tarefa';
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private taskService: TaskService
+  ) {}
+
+   ngOnInit() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id) {
+      this.task = this.taskService.getById(parseInt(id));
+      this.title = 'Alterando tarefa';
+    }
+   }
+
+   onSubmit() {
+
+   }
 
 }
